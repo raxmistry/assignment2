4ca76b8ab5b1d59045baf43a1f84eaaecc433415
Rakesh <rakesh.mistry@gmail.com>
2014-07-25 22:57:16 +0200

    Starting client code

--- server/src/main/java/com/bsg/assignment2/server/Server.java
+++ server/src/main/java/com/bsg/assignment2/server/Server.java
@@ -1,5 +1,7 @@
 package com.bsg.assignment2.server;
 
+import com.bsg.assignment2.common.CommunicationWrapper;
+
 /**
  * Created by rmistry on 2014/07/25.
  */
--- server/src/main/java/com/bsg/assignment2/server/ServerImpl.java
+++ server/src/main/java/com/bsg/assignment2/server/ServerImpl.java
@@ -1,12 +1,13 @@
 package com.bsg.assignment2.server;
 
+import com.bsg.assignment2.common.CommunicationWrapper;
+
 /**
  * Created by rmistry on 2014/07/25.
  */
 public class ServerImpl implements Server {
 
     CommunicationWrapper communicationWrapper;
-
 
     @Override
     public void setCommunicationWrapper(CommunicationWrapper communicationWrapper) {
